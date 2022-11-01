8b0cb439f0b5e484c7d8c6220cdf91ef34dababc
Gabipretel <gabypretel@gmail.com>
2022-11-01 20:16:26 -0300

    Cambios rutas,db,modelos

--- api/src/db.js
+++ api/src/db.js
@@ -4,13 +4,13 @@ const fs = require('fs')
 const path = require('path')
 
 
- const {
-   DB_USER, DB_PASSWORD, DB_HOST
- } = process.env;
- const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pf-demo`, {
-         logging: false,
-         native: false,
-       });
+  const {
+    DB_USER, DB_PASSWORD, DB_HOST
+  } = process.env;
+  const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pf-demo`, {
+          logging: false,
+          native: false,
+        });
 
 
 // const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
