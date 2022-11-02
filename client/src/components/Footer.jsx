62b9eacdc77daa2967637b75d9d8bc9cfe8a89b4
red 1730 <ducret.raul@gmail>
2022-11-02 01:28:56 -0300

    footer2

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,12 +1,15 @@
 import { Container } from '@mui/system';
 import { Grid } from '@mui/material';
 import Box from '@mui/material/Box';
-import Link from "@mui/material/Link";
+import Link from '@mui/material/Link';
 
 export const Footer_comp = () => {
   return (
-    <Box px= {{xs:3, sm:10}} py={{xs:5, sm:10}} bgcolor="text.secondary" color= "white">
-      <Container maxWidth="lg">
+    <Box sx={{ position: '-webkit-sticky', bottom: 0 }}
+      px={{ xs: 3, sm: 8 }}
+      py={{ xs: 5, sm: 3 }}
+      bgcolor="text.secondary" color="white" >
+      <Container maxWidth="xl">
         <Grid container spacing={5}>
           <Grid item xs={12} sm={4}>
             <Box borderBottom={1}>Ayuda</Box>
@@ -53,9 +56,10 @@ export const Footer_comp = () => {
             </Box>
           </Grid>
         </Grid>
-        <Box textAlign="center" pt={{xs:5, sm:10}} pb={{xs:5, sm:0}}>
+        <Box textAlign="center" pt={{ xs: 5, sm: 10 }} pb={{ xs: 5, sm: 0 }}>
+          <Link  href="/catalogo" color="primary.main">
           Healty Food &reg; {new Date().getFullYear()}
-
+          </Link>
         </Box>
       </Container>
     </Box>
