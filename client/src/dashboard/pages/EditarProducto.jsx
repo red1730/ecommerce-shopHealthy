ea50921f30fe0db3c4de0c8612efd0dcaac6fd07
xchristx <cheisagui@gmail.com>
2022-11-02 01:19:56 -0400

    fix: editar producto

--- client/src/dashboard/pages/EditarProducto.jsx
+++ client/src/dashboard/pages/EditarProducto.jsx
@@ -1,5 +1,7 @@
-import { Box, Card, Link, Typography, Stack, capitalize, Button } from '@mui/material';
-import { styled } from '@mui/material/styles';
+import React from 'react';
+import { Box, Card, Link, Typography, Stack, capitalize, Button, TextField, Alert, InputLabel, FormControl } from '@mui/material';
+import { styled,alpha } from '@mui/material/styles';
+import InputBase from '@mui/material/InputBase';
 // utils
 import { fCurrency } from '../utils/formatNumber';
 // components
@@ -8,6 +10,8 @@ import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { getProductById } from '../../helpers/getProductById';
+import { useForm, Controller } from "react-hook-form";
+import { LoadingButton } from '@mui/lab';
 
 // ----------------------------------------------------------------------
 
@@ -18,7 +22,30 @@ const StyledProductImg = styled('img')({
   objectFit: 'cover',
   position: 'absolute',
 });
+const RedditTextField = styled(React.forwardRef((props, ref) => (
+    <TextField ref={ref} InputProps={{ disableUnderline: true }} {...props} />
+  )))(({ theme }) => ({
+    '& .MuiFilledInput-root': {
+      border: '1px solid #e2e2e1',
 
+      overflow: 'hidden',
+      borderRadius: 4,
+      backgroundColor: theme.palette.mode === 'light' ? '#fcfcfb' : '#2b2b2b',
+      transition: theme.transitions.create([
+        'border-color',
+        'background-color',
+        'box-shadow',
+      ]),
+      '&:hover': {
+        backgroundColor: 'transparent',
+      },
+      '&.Mui-focused': {
+        backgroundColor: 'transparent',
+        boxShadow: `${alpha(theme.palette.primary.main, 0.25)} 0 0 0 2px`,
+        borderColor: theme.palette.primary.main,
+      },
+    },
+  }));
 // ----------------------------------------------------------------------
 
 // ShopProductCard.propTypes = {
@@ -49,14 +76,29 @@ export default function EditarProducto() {
     getProduct();
   }, [id, dispatch, navigate]);
     
-  const { nombre, precio, img, stock, activo } = product;
+  const { nombre, precio, img, stock, activo, descripcion } = product;
+  console.log(nombre)
 
+  const { handleSubmit, formState:{errors}, control } = useForm({
+    defaultValues:{
+        nombre:nombre,
+        precio,
+        stock,
+        activo,
+        descripcion
+    }
+  });
+  const onSubmit = e=>{
+    e.preventDefault();
+    console.log(e)
+  }
 
   if(isLoading) return <Typography variant='h3' m='0 auto' >Cargando...</Typography>
 
   return (
-    <Card sx={{boxShadow:15, width:'40%',m:'0 auto'}} >
-      <Box sx={{ pt: '100%', position: 'relative' }}>
+    <>
+    <Card sx={{boxShadow:15, width:{xs:'100%',md:'40%'}, m:'0 auto'}} >
+      <Box sx={{ pt: '100%', position: 'relative', }}>
         
           {!activo && <Label
             variant="filled"
@@ -74,32 +116,74 @@ export default function EditarProducto() {
         
         <StyledProductImg alt={nombre} src={`https://dkndrd.com/pf-healthyShop/${img}`} />
       </Box>
-      <Stack spacing={2} sx={{ p: 3 }}>
-        {/* <Link color="inherit" underline="hover"> */}
-          <Typography variant="subtitle2" noWrap>
-            {nombre.toUpperCase()}
-          </Typography>
-        {/* </Link> */}
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <Stack spacing={1}>
+                <Controller 
+                    name="nombre"
+                    control={control}
+                    rules={{ required: true }}
+                    render={({ field }) => <RedditTextField
+                                            label="Nombre"
+                                            id="nombre"
+                                            variant="filled"
+                                            style={{ marginTop: 11 }}
+                                            {...field}
+                                        />}
+                
+                />
+                {errors.nombre?.type === 'required' &&  <Alert sx={{height:'25px', p:0, m:0}} severity="error">El nombre es requerido</Alert>}
+                <Controller 
+                    name="marca"
+                    control={control}
+                    rules={{ required: true }}
+                    render={({ field }) => <RedditTextField
+                                            label="Precio"
+                                            defaultValue={precio}
+                                            id="precio"
+                                            variant="filled"
+                                            style={{ marginTop: 11 }}
+                                            {...field}
+                                        />}
+                
+                />
+                {errors.nombre?.type === 'required' &&  <Alert sx={{height:'25px', p:0, m:0}} severity="error">El nombre es requerido</Alert>}
+                <Controller 
+                    name="categoria"
+                    control={control}
+                    rules={{ required: true }}
+                    render={({ field }) => <RedditTextField
+                                            label="Stock"
+                                            defaultValue={stock}
+                                            id="stock"
+                                            variant="filled"
+                                            style={{ marginTop: 11 }}
+                                            {...field}
+                                        />}
+                
+                />
+                {errors.nombre?.type === 'required' &&  <Alert sx={{height:'25px', p:0, m:0}} severity="error">El nombre es requerido</Alert>}
+                <Controller 
+                    name="descripcion"
+                    control={control}
+                    rules={{ required: true }}
+                    render={({ field }) => <RedditTextField
+                                            label='Activo:'
+                                            defaultValue={activo}
+                                            id="activo"
+                                            variant="filled"
+                                            style={{ marginTop: 11 }}
+                                            {...field}
+                                        />}
+                
+                />
+                {errors.nombre?.type === 'required' &&  <Alert sx={{height:'25px', p:0, m:0}} severity="error">El nombre es requerido</Alert>}
+                {/* <LoadingButton fullWidth size="large" type="submit" variant="contained">
+                    Login
+                </LoadingButton> */}
 
-        <Stack direction="row" alignItems="center" justifyContent="space-between">
-          {/* <ColorPreview colors={colors} /> */}
-          <Button variant='contained' >Editar</Button>
-          <Typography variant="subtitle1">
-            <Typography
-              component="span"
-              variant="body1"
-              sx={{
-                color: 'text.disabled',
-                textDecoration: 'line-through',
-              }}
-            >
-              {/* {priceSale && fCurrency(priceSale)} */}
-            </Typography>
-            &nbsp;
-            {fCurrency(precio)}
-          </Typography>
-        </Stack>
-      </Stack>
+            </Stack>
+        </form>
     </Card>
+    </>
   );
 }
