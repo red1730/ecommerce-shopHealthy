ccdc4b3e29acd65a7144fce4d0e377ffcae64cfe
Gabipretel <gabypretel@gmail.com>
2022-11-04 19:18:25 -0300

    update Register validación

--- client/src/auth/authReducer.js
+++ client/src/auth/authReducer.js
@@ -1,9 +1,9 @@
 import { type } from "../../types/index"
 
-const state = {
-  name: "Henry",
-  logged: true,
-};
+// const state = {
+//   name: "Henry",
+//   logged: true,
+// };
 
 export const authReducer = (state = {}, action) => {
   switch (action.type) {
