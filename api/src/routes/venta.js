bd6cf4b8fae6474fe55503cd8471bfc0ea14cc48
Christian <christhs@vps.server.local>
2022-11-10 18:07:16 +0000

    ruta venta

--- api/src/routes/venta.js
+++ api/src/routes/venta.js
@@ -8,6 +8,60 @@ const { Review } = require('../db')
 
 const router = Router();
 
+//GET TODAS LAS VENTAS
+router.get("/", async (req, res) => {
+
+
+  let vta = await Venta.findAll({
+      attributes: ['id', 'fecha', 'precioTotal','estadoEnvio','usuarioId'],
+      include: { 
+          model: Detalleventa,
+          attributes: ['id', 'cantidad', 'precioUnitario'],
+          include:{ 
+              model: Producto,
+              attributes: ['id', 'nombre', 'precio','img','stock','descripcion','marcaId'],
+              include: [{ 
+                  model: Categoria,
+                  attributes: ['nombre']
+                },{
+                  model: Marca,
+                  attributes: ['nombre']
+                },{
+                  model: Review,
+                  attributes: ['puntaje', 'titulo', 'comentario']
+
+                }],
+            }
+
+
+      }
+  })
+
+  res.send(vta)
+
+})
+//PUT VENTA, CAMBIA EL ESTADO, RECIBE ID VENTA
+router.put("/:id", async (req, res) => {
+  try {
+
+    let id = req.params.id;  //id venta
+
+    const venta = await Venta.findByPk(id);
+    const { estadoEnvio } = req.body;
+
+
+    if (estadoEnvio) {
+      venta.estadoEnvio = estadoEnvio;
+      venta.save();
+    }
+
+    res.status(200).send(`El estado de la venta ${id} fue cambiado a ${estadoEnvio}`);
+  } catch (error) {
+    console.log(error)
+    res.status(400).send(error);
+
+  }
+});
 
 
   router.get("/:id", async (req, res) => {
